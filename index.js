df887b015b03e344689ef3b5b8a480a374a4145a
malcomowilla <owillamalcom@gmail.com>
2022-11-28 21:07:05 +0300

    completed assighnment

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // code your solution here
+
+function saturdayFun (){
+
+}
+
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun(activity = "bathe my dog");
+
+
+function mondayWork (){
+
+}
+
+function mondayWork (){
+    return (mondayWork);
+
+
+}
+mondayWork('go to the office');
+
+
+
+function mondayWork (activity = "go to the office"){
+    return `This Monday, I will ${activity}.`
+}
+
+functionwrapAdjective (wrap="+") {
+    return function(adjective="special") {
+        return `You are ${wrap}${adjective}${wrap}!`
+    }
+}
+
+
+
